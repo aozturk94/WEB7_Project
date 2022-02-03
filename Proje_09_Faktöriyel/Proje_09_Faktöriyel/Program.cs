921cdb47f52c42e4505cf32dec3545b34587df47
aozturk94 <ahmetozturk94@outlook.com>
2022-02-03 21:01:38 +0300

    Düzenle

---
+++ Proje_09_Faktöriyel/Proje_09_Faktöriyel/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Proje_09_Faktöriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = 1;
+            Console.WriteLine("Sayıyı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc = sonuc * i;
+            }
+            Console.WriteLine($"{sayi}!= {sonuc}");
+            Console.ReadLine();
+        }
+    }
+}
