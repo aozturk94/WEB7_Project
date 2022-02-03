931acf065d827b3f22c7bda0fd13f2f23defff43
aozturk94 <ahmetozturk94@outlook.com>
2022-02-03 18:02:21 +0300

    Proje 8 tamamlandı.

--- Proje_08_For/Proje_08_For/Program.cs
+++ Proje_08_For/Proje_08_For/Program.cs
@@ -41,7 +41,7 @@ namespace Proje_08_For
 
             /*1 ile 100 arasındaki asal sayıları ekrana yazdıran programı yazınız.*/
 
-            /*            bool asal = true;  //!!!İkili durumlarda bool kullanmak kolaylık sağlar.!!!
+            /*          bool asal = true;  //!!!İkili durumlarda bool kullanmak kolaylık sağlar.!!!
                         for (int i = 2; i <=100; i++) //Asal olup olmadığını kontrol edeceğimiz sayıları temsil eden döngü.
                         {
                             for (int j = 2; j < i; j++) //"i" sayısının asal olup olmadığını kontrol etmek için kendisine kadar olan sayılara böldüğümüz döngü.
@@ -62,7 +62,7 @@ namespace Proje_08_For
                         }
 
                         Console.ReadLine();*/
-            int say = 0;
+            /*int say = 0;
             int say2 = 0;
             int say3 = 0;
             bool asal = true;  //!!!İkili durumlarda bool kullanmak kolaylık sağlar.!!!
@@ -103,7 +103,24 @@ namespace Proje_08_For
             Console.WriteLine($"1 ile 100 arasındaki asal sayı adedi: {say}");
             Console.WriteLine($"100 ile 200 arasındaki asal sayı adedi: {say2}");
             Console.WriteLine($"200 ile 300 arasındaki asal sayı adedi: {say3}");
+            Console.ReadLine();*/
+
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Title = "ÇARPIM TABLOSU";
+
+            for (int i = 1; i <=10; i++)
+            {
+                for (int j = 1; j <=10; j++)
+                {
+                    Console.Write($"{i}x{j}={i*j}\t");
+                }
+
+                Console.WriteLine("\n");
+            }
+
             Console.ReadLine();
+
 
 
         }
