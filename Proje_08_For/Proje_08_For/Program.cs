13fb1b312c25e7a1936b5a96db3774cb53ef3c08
aozturk94 <ahmetozturk94@outlook.com>
2022-02-03 15:41:59 +0300

    Proje 8'i ekledim.

---
+++ Proje_08_For/Proje_08_For/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proje_08_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
