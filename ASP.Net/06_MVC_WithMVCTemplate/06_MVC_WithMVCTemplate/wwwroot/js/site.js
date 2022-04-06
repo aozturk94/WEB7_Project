00d1f7429c1ccb508b86a12e96527a45da726a20
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-06 16:11:02 +0300

    'hover' hatası?

--- ASP.Net/06_MVC_WithMVCTemplate/06_MVC_WithMVCTemplate/wwwroot/js/site.js
+++ ASP.Net/06_MVC_WithMVCTemplate/06_MVC_WithMVCTemplate/wwwroot/js/site.js
@@ -1,6 +1,13 @@
-﻿$(document).ready(function () {
+﻿/*$(document).ready(function () {
     var pathName = window.location.pathname;
     console.log(pathName);
-    /*    $("li a").removeClass("activeEt")*/
     $('ul  li  a[href="' + pathName + '"]').addClass("activeEt").siblings().removeClass("activeEt");
-})
+});*/ //Alttakinin sadesi sayılır?
+
+$(document).ready(function () {
+    $("li a").each(function () {
+        if ($(this).attr("href") == window.location.pathname) {
+            $(this).addClass("activeEt");
+        }
+    })
+});
