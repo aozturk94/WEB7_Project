ed1666eab6d51fc26ae1c29c315fa76e36038447
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-08 18:30:34 +0300

    done!

---
+++ ASP.Net/MobilyaShowRoom/MobilyaShowRoom/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobilyaShowRoom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilyaShowRoom.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = Repository.AllProduct();
+            return View(products);
+        }
+
+        public IActionResult GetProductDetail()
+        {
+            int id = Convert.ToInt32(ControllerContext.RouteData.Values["id"]);
+            List<Product> products = Repository.AllProduct();
+            Product resultProduct = null;
+
+            foreach (var product in products)
+            {
+                if (product.ID==id)
+                {
+                    resultProduct = product;
+                    break;
+                }
+            }
+
+            ViewBag.ResultProduct = resultProduct;
+            return View();
+        }
+    }
+}
---
+++ ASP.Net/MobilyaShowRoom/MobilyaShowRoom/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilyaShowRoom.Models
+{
+    public static class Repository
+    {
+        public static List<Product> AllProduct()
+        {
+            var products = new List<Product>
+            {
+                new Product {ID=470, ProductName="Dolap", ProductDetail="İki kapılı ahşap dolap1", Price=5000},
+                new Product {ID=570, ProductName="Koltuk", ProductDetail="İki kapılı ahşap dolap2", Price=6000},
+                new Product {ID=670, ProductName="TV Sehpası", ProductDetail="İki kapılı ahşap dolap3", Price=7000},
+                new Product {ID=430, ProductName="Yatak", ProductDetail="İki kapılı ahşap dolap4", Price=8000},
+                new Product {ID=478, ProductName="Masa", ProductDetail="İki kapılı ahşap dolap5", Price=9000},
+                new Product {ID=180, ProductName="Sandalye", ProductDetail="İki kapılı ahşap dolap6", Price=10000}
+            };
+            return products;
+        }
+    }
+}
