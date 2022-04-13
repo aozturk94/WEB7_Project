7e1f3d8944e7b750536645e919e15d240214d0bb
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-13 19:58:57 +0300

    update

--- ASP.Net/DikenComTr_Clone_Sınıf/DikenComTr_Clone/wwwroot/js/site.js
+++ ASP.Net/DikenComTr_Clone_Sınıf/DikenComTr_Clone/wwwroot/js/site.js
@@ -2,5 +2,11 @@
     $("#menu-btn").click(function () {
         $(".nav-content").toggle("slow");
     })
-
 })
+
+
+var f = document.getElementById('foo');
+document.addEventListener('click', function (ev) {
+    f.style.transform = 'translateY(' + (ev.clientY - 25) + 'px)';
+    f.style.transform += 'translateX(' + (ev.clientX - 25) + 'px)';
+}, false);
