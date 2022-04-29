beba1b969cb7cdd20e420337078fad403a0fd7a1
aozturk94 <98884819+aozturk94@users.noreply.github.com>
2022-04-29 19:23:08 +0300

    update

--- Entity Framework/UploadImageProject/UploadImageProject/Controllers/HomeController.cs
+++ Entity Framework/UploadImageProject/UploadImageProject/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using UploadImageProject.Models;
 
 namespace UploadImageProject.Controllers
 {
@@ -15,18 +16,28 @@ namespace UploadImageProject.Controllers
         public IActionResult Index()
         {
             return View();
-        }  
-        
+        }   
         public IActionResult UploadImage()
         {
-
-            return View();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/images/");
+            var imageList = Directory.GetFiles(path);
+            List<UploadedImage> uploadedImages = new List<UploadedImage>();
+            foreach (var image in imageList)
+            {
+                FileInfo fileInfo = new FileInfo(image);
+                UploadedImage uploadedImage = new UploadedImage();
+                uploadedImage.ImageFullName = fileInfo.FullName.Substring(fileInfo.FullName.IndexOf("wwwroot")).Replace("wwwroot", string.Empty);
+                uploadedImage.ImageName = fileInfo.Name;
+                uploadedImage.ImageSize = fileInfo.Length/1024;
+                uploadedImages.Add(uploadedImage);
+            }
+            return View(uploadedImages);
         }
 
         [HttpPost]
         public IActionResult UploadImage(IFormFile file)
         {
-            if(file != null && file.ContentType == "image/png")//eğer file boş değilse, yani dosya seçilmişse
+            if(file != null )//eğer file boş değilse, yani dosya seçilmişse
             {
                 string imageExtension = Path.GetExtension(file.FileName);
                 string imageName = Guid.NewGuid() + imageExtension;
